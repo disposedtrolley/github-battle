ec4b4b29b465645253a06859561d79a8dcdbac73
James Liu <james_liu@fastmail.com>
2017-05-21 09:51:18 +1000

    added RepoGrid component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -29,6 +29,35 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired,
 };
 
+function RepoGrid(props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function(repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login} />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 
 class Popular extends React.Component {
   constructor(props) {
@@ -69,7 +98,9 @@ class Popular extends React.Component {
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
-        <RepoGrid repos={this.state.repos} />
+        {!this.state.repos
+          ? <p>LOADING</p>
+          : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
