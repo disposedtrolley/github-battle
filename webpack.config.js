5b34b059f9f14c164392c06f396e4140d6aa06eb
James Liu <james_liu@fastmail.com>
2017-05-21 11:46:10 +1000

    modified webpack config to allow requests to /<path> directly from URL

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
+  },
+  devServer: {
+    // will redirect any GET requests to /x to /
+    // then React Router will route to the correct path
+    historyApiFallback: true
   },
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
